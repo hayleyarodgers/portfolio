7e608d241a6b52e07524a7fd342a9642656c818f
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-03 23:10:52 +1100

    Work part-way through showing error message if field is left empty

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "../../styles/contact.css";
 
 import linkedinLogo from "../../assets/images/linkedinLogo.png";
 import githubLogo from "../../assets/images/githubLogo.png";
 import emailIcon from "../../assets/images/emailIcon.png";
 
-import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+import { validateEmail } from "../../utils/helpers";
+
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [nameErrorMessage, setNameErrorMessage] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [messageErrorMessage, setMessageErrorMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, set the state of the field
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  // Check fields aren't empty and email is valid
+  if (!name) {
+    setNameErrorMessage("Please add your name.");
+    return;
+  }
+  if (!email || !validateEmail(email)) {
+    setEmailErrorMessage("Please add a valid email address.");
+    return;
+  }
+  if (!message) {
+    setMessageErrorMessage("Please add your message.");
+    return;
+  }
+
+  // Note: no back end so unable to code for submitting messages yet.
+  const handleFormSubmit = (event) => {
+    // Prevent page refresh
+    event.preventDefault();
+
+    // Coming soon
+
+    // Clear form after a successful message
+    setName("");
+    setEmail("");
+    setMessage("");
+    setNameErrorMessage("");
+    setEmailErrorMessage("");
+    setMessageErrorMessage("");
+  };
+
   return (
     <main className="contact">
       <h2>Contact me</h2>
@@ -60,16 +115,40 @@ export default function Contact() {
       <br />
       <Form className="w-50 mx-auto">
         <h3>Or, get in touch directly!</h3>
-        <FloatingLabel label="Your name" className="mb-3">
-          <Form.Control type="text" />
-        </FloatingLabel>
-        <FloatingLabel label="Your email address" className="mb-3">
-          <Form.Control type="email" />
-        </FloatingLabel>
-        <FloatingLabel label="Your message" className="mb-3">
-          <Form.Control as="textarea" />
-        </FloatingLabel>
-        <Button variant="primary" type="submit">
+        <Form.Control
+          type="text"
+          name="name"
+          className="mb-3"
+          onChange={handleInputChange}
+          placeholder="Your name"
+          aria-describedby="nameHelpBlock"
+        />
+        <Form.Text id="nameHelpBlock" muted>
+          {nameErrorMessage}
+        </Form.Text>
+        <Form.Control
+          type="email"
+          name="email"
+          className="mb-3"
+          onChange={handleInputChange}
+          placeholder="Your email"
+          aria-describedby="emailHelpBlock"
+        />
+        <Form.Text id="emailHelpBlock" muted>
+          {emailErrorMessage}
+        </Form.Text>
+        <Form.Control
+          as="textarea"
+          name="message"
+          className="mb-3"
+          onChange={handleInputChange}
+          placeholder="Your message"
+          aria-describedby="messageHelpBlock"
+        />
+        <Form.Text id="messageHelpBlock" muted>
+          {messageErrorMessage}
+        </Form.Text>
+        <Button variant="primary" type="submit" onClick={handleFormSubmit}>
           Send
         </Button>
       </Form>
