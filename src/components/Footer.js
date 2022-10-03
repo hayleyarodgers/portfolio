1f8aa7c0cbae6b62824006cdada2429dff1b593a
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-03 14:56:42 +1100

    Write Footer component and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import linkedinLogo from '../assets/images/linkedinLogo.png';
+import githubLogo from '../assets/images/githubLogo.png';
+import emailLogo from '../assets/images/emailLogo.png';
+import '../styles/footer.css';
 
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer text</p>
+    <footer className="footer">
+      <div id="contact-bar">
+        <a
+          href="https://www.linkedin.com/in/hayleyarodgers/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src={linkedinLogo} alt="LinkedIn logo" />
+        </a>
+        <a
+          href="https://github.com/hayleyarodgers"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src={githubLogo} alt="GitHub logo" />
+        </a>
+        <a
+          href="mailto:hayleyarodgers88@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src={emailLogo} alt="Email logo" />
+        </a>
+      </div>
+      <i>
+        Made using React on Gadigal Land. To see the source code, click
+        <a href="https://github.com/hayleyarodgers/portfolio"> here</a>.
+      </i>
     </footer>
   );
 }
