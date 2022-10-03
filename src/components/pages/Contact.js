673ae59f8730dcbff30c0fa0c3c3051b85dd6a15
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-03 15:16:07 +1100

    Write Contact Component and styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import '../../styles/contact.css';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>x</p>
-    </div>
+    <main className="contact">
+      <h2>Contact me</h2>
+      <br />
+      <h3>LinkedIn</h3>
+      <a href="https://www.linkedin.com/in/hayleyarodgers/">
+        https://www.linkedin.com/in/hayleyarodgers/
+      </a>
+      <br />
+      <br />
+      <h3>GitHub</h3>
+      <a href="https://github.com/hayleyarodgers">
+        https://github.com/hayleyarodgers
+      </a>
+      <br />
+      <br />
+      <h3>Email</h3>
+      <a href="mailto:hayleyarodgers88@gmail.com">hayleyarodgers88@gmail.com</a>
+      <br />
+      <br />
+      <h3>Or message me directly!</h3>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6 mx-auto">
+            <form className="contact-form">
+              <label className="d-flex justify-content-between my-4" for="name">
+                Name
+                <input
+                  className="w-75 form-control"
+                  id="name"
+                  name="name"
+                  type="text"
+                />
+              </label>
+              <label
+                className="d-flex justify-content-between my-4"
+                for="email">
+                Email
+                <input
+                  className="w-75 form-control"
+                  id="email"
+                  name="email"
+                  type="email"
+                />
+              </label>
+              <label
+                className="d-flex justify-content-between my-4"
+                for="message">
+                Message
+                <textarea
+                  rows="3"
+                  className="w-75 form-control"
+                  id="message"
+                  placeholder="Write your message here."></textarea>
+              </label>
+              <button className="btn btn-lg btn-primary rounded-0 my-4">
+                SEND
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
