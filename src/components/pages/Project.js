2249fe22d04a3fa7e7ff5790615996691b28e98d
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-04 22:14:56 +1100

    Make portfolio image a clickable link to deployed project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../../styles/project.css";
+
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -11,12 +13,14 @@ export default function Project(props) {
       {props.projects.map((project) => (
         <Col key={project.id}>
           <Card className="rounded-0 h-100">
-            <Card.Img
-              variant="top"
-              src={project.screenshot}
-              alt="Project screenshot"
-              className="rounded-0"
-            />
+            <a href={project.demoURL} target="_blank" rel="noopener noreferrer">
+              <Card.Img
+                variant="top"
+                src={project.screenshot}
+                alt="Project screenshot"
+                className="rounded-0 greyscaleImg"
+              />
+            </a>
             <Card.Body>
               <h3>{project.name}</h3>
               <h4>{project.type}</h4>
@@ -29,15 +33,17 @@ export default function Project(props) {
                 variant="primary"
                 href={project.repoURL}
                 target="_blank"
-                rel="noopener noreferrer">
+                rel="noopener noreferrer"
+                className="me-2">
                 CODE
               </Button>{" "}
               {project.demoURL ? (
                 <Button
                   variant="primary"
-                  href={project.liveAppURL}
+                  href={project.demoURL}
                   target="_blank"
-                  rel="noopener noreferrer">
+                  rel="noopener noreferrer"
+                  className="ms-2">
                   DEMO
                 </Button>
               ) : (
